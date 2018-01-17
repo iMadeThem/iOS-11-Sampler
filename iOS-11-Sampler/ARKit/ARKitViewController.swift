968073b2ac27c4f7628c2cb795d2edb1c9f364b5
Leon <yong.he1982@gmail.com>
2018-01-17 18:09:31 +0900

    Fix for build errors.

--- iOS-11-Sampler/ARKit/ARKitViewController.swift
+++ iOS-11-Sampler/ARKit/ARKitViewController.swift
@@ -25,7 +25,7 @@ class ARKitViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let configuration = ARWorldTrackingSessionConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         sceneView.session.run(configuration)
     }
     
